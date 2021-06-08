40c112d07497583bbe975cd7a45f6ef28cd395fb
JuanMartin-Garcia <you@example.com>
2021-06-08 18:00:29 -0300

    Aca va el Register por Primera Vez

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
-app.use(express.static(__dirname +'/public'));  
+
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
 
 app.listen(3000, () => {
     console.log("Servidor corriendo")
@@ -10,8 +14,11 @@ app.listen(3000, () => {
 
 app.get("/", (req,res) =>{
     //res.send("Hola Mundo");  // Permite enviar texto o codigo HTML
-    let pathHTML =path.join(__dirname , "/views/home.html")
-    res.sendFile(pathHTML) // Permite enviar un archivo HTML
+    res.sendFile(__dirname + "/views/home.html")
+ // Permite enviar un archivo HTML
 });
 
+app.get("/register", function(req,res){
+    res.sendFile(__dirname + "/views/register.html")
 
+});
