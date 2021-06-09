0160575cb4ae450c46b651cc46e9209d04fba93d
IndustrialBot <juano94@gmail.com>
2021-06-08 21:50:01 -0300

    probando deploy

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo")
-});
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
 
 app.get("/", (req,res) =>{
     //res.send("Hola Mundo");  // Permite enviar texto o codigo HTML
